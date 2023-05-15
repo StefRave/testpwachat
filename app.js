904a454d9019ed43187d0c164654839447fd4371
Stef Rave <stef.rave@gmail.com>
2023-05-15 22:02:45 +0200

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const registerServiceWorker = async () => {
+  try {
+    await navigator.serviceWorker.register('sw.js', { scope: '/testpwachat/'});
+    console.log('Service worker registered');
+  } catch (e) {
+    console.log(`Registration failed: ${e}`);
+  }
+}
+
+if (navigator.serviceWorker) {
+  registerServiceWorker();
+}
